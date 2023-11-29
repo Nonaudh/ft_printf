f05eedaa61ef01f7aae0d1dde59362b93558cefa
Nonaudh <nonaudh@gmail.com>
2023-11-29 15:12:03 +0100

    ok

--- ft_printf.c
+++ ft_printf.c
@@ -15,22 +15,85 @@ int	print_str(char *str)
 
 	i = 0;
 	while (str[i])
-	{
-		write(1, &str[i], 1);
 		i++;
-	}
+	write (1, str, i);
 	return (i);
 }
 
 int	print_number(int n)
 {
-	char	*number;
-	int	lenght;
-	
-	number = ft_itoa(n);
-	lenght = print_str(number);
-	return (lenght);
+	long	nb;
+	nb = (long)n;
+
+	if (nb < 0)
+	{
+		print_char('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		print_number(nb / 10);
+		nb = nb % 10;
+	}
+	print_char(nb + 48);
+	return (1);
 }
+
+int	print_unsigned(int n)
+{
+	long	nb;
+	nb = (long)n;
+
+	if (nb < 0)
+		nb = 4294967296 + nb;
+	if (nb >= 10)
+	{
+		print_unsigned(nb / 10);
+		nb = nb % 10;
+	}
+	print_char(nb + 48);
+	return (1);
+}
+
+int	print_HEXAnumber(int n)
+{
+	long	nb;
+	nb = (long)n;
+
+	if (nb < 0)
+		nb = 4294967296 + nb;
+	if (nb >= 16)
+	{
+		print_HEXAnumber(nb / 16);
+		nb = nb % 16;
+	}
+	if (nb < 10)
+		print_char(nb + 48);
+	if (nb > 9)
+		print_char(nb + 55);
+	return (1);
+}
+
+int	print_hexanumber(int n)
+{
+	long	nb;
+	nb = (long)n;
+
+	if (nb < 0)
+		nb = 4294967296 + nb;
+	if (nb >= 16)
+	{
+		print_hexanumber(nb / 16);
+		nb = nb % 16;
+	}
+	if (nb < 10)
+		print_char(nb + 48);
+	if (nb > 9)
+		print_char(nb + 87);
+	return (1);
+}
+
+
 
 int	print_percent(void)
 {
@@ -49,6 +112,15 @@ int	set_format(va_list args, char c)
 
 	if (c == 'd' || c == 'i')
 		return (print_number(va_arg(args, int)));
+
+	if (c == 'u')
+		return (print_unsigned(va_arg(args, int)));
+
+	if (c == 'X')
+		return (print_HEXAnumber(va_arg(args, int)));
+
+	if (c == 'x')
+		return (print_hexanumber(va_arg(args, int)));
 
 	if (c == '%')
 		return (print_percent());
@@ -84,8 +156,14 @@ int	ft_printf(const char *str, ...)
 	return (print);
 }
 
+#include <limits.h>
+
 int main ()
 {
+	int *test;
+	int nb = 1999;
+	test = &nb;
 
-	printf("%u\n", 4294967293);
+	printf("%p\n", (void *)test);
+	ft_printf("%x", (void *)test);
 }
